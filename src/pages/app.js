f5f69830b55aa8bdcace55a557f8ee991734993a
Matt Oliver <mattoliver.mattoliver@gmail.com>
2022-07-06 12:43:25 -0600

    wip

--- src/pages/app.js
+++ src/pages/app.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Router } from "@reach/router";
 import Layout from "../components/Layout";
 import Profile from "../components/Profile";
-import Login from "../components/Login";
 
 const App = () => (
   <Layout>
