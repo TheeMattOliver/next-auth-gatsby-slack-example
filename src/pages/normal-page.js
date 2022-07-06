e142e2cc83d49da3b7bdc4ed6865a6d8c96e3261
Matt Oliver <mattoliver.mattoliver@gmail.com>
2022-07-06 09:49:33 -0500

    normal page

---
+++ src/pages/normal-page.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NormalPage() {
+  return (
+    <>
+      <h1>Normal</h1>
+      <p>You can view this page if you are not signed in.</p>
+    </>
+  );
+}
