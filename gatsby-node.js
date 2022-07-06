a2e7b5ef35443a45da89fcd96f35a6b47d64fdc2
Matt Oliver <mattoliver.mattoliver@gmail.com>
2022-07-06 14:08:47 -0600

    log gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,8 @@
 // Implement the Gatsby API “onCreatePage”. This is
 // called after every page is created.
-// wip
+
 exports.onCreatePage = async ({ page, actions }) => {
+  console.log("creating pages....");
   const { createPage } = actions;
   // page.matchPath is a special key that's used for matching pages
   // only on the client.
