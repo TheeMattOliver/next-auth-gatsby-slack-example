98bd7f619aeb31d97b2cf7a823eb97abface05bb
Matt Oliver <mattoliver.mattoliver@gmail.com>
2022-07-07 23:03:22 -0500

    wipg

--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Router } from "@reach/router";
-import Layout from "../components/Layout";
-import PrivateRoute from "../components/PrivateRoute";
-import Profile from "../components/Profile";
-import Login from "../components/Login";
+import Layout from "../../components/Layout";
+import PrivateRoute from "../../components/PrivateRoute";
+import Profile from "../../components/Profile";
+import Login from "../../components/Login";
 
 const App = () => (
   <Layout>
