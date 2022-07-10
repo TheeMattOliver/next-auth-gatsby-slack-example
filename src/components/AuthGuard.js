d07e3f8311d896f48bf8fc5c9d1fe5421c3a4809
Matt Oliver <mattoliver.mattoliver@gmail.com>
2022-07-09 19:21:59 -0500

    adding children if authenticated

--- src/components/AuthGuard.js
+++ src/components/AuthGuard.js
@@ -9,7 +9,7 @@ export default function AuthGuard({ children }) {
       {
         {
           loading: <p>Loading session...</p>,
-          authenticated: { children },
+          authenticated: children,
           unauthenticated: "Please sign in",
         }[session?.status ?? "loading"]
       }
